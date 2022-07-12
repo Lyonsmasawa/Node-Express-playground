622a3bca109c79068f7e00903173c9fcf81ee63e
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-07-12 04:59:22 -0700

    stream read display in node

--- app.js
+++ app.js
@@ -288,9 +288,9 @@ const startNative = async() => {
 
     // READ FILE
     const { createReadStream } = require('fs');
-    // const stream = createReadStream('./content/big.txt', )
+    const stream = createReadStream('./content/big.txt', )
     // const stream = createReadStream('./content/big.txt', {encoding: 'utf8'})
-    const stream = createReadStream('./content/big.txt', { highWaterMark: 90000, encoding: 'utf-8' })
+    // const stream = createReadStream('./content/big.txt', { highWaterMark: 90000, encoding: 'utf-8' })
 
     stream.on('data', (result) => {
         console.log(result);
