0b80c9ce80d5de771ecc3d51784c5b10b8af1b7a
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-07-11 21:19:35 -0700

    intro

---
+++ node.js
@@ -0,0 +1,14 @@
+// Install node and check node --version
+// node - repl (acts as a console)
+// node app.js - node cli
+
+const amount = 12;
+
+if(amount < 10) {
+    console.log("small number");
+}
+else {
+    console.log("hello world!")
+}
+
+// GLOBAL VARIABLES IN NODE
