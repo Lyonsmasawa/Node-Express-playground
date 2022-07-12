0b52ed1fd6a63457eace525fa8183d0a59a22598
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-07-11 23:16:37 -0700

    Fs Async built in module

--- app.js
+++ app.js
@@ -93,3 +93,26 @@ writeFileSync('./content/result-sync.txt', `hello world!, : ${first, second}`, {
 
 // Async
 const { readFile, writeFile } = require('fs')
+
+readFile('./content/first.txt', 'utf8', (err, result) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+    // console.log(result)
+    const first = result
+    readFile('./content/second.txt', 'utf8', (err, result) => {
+        if (err) {
+            console.log(err)
+            return
+        }
+        const second = result
+        writeFile('./content/result-async.txt', `${ first, second }`, {flag: 'a'},  (err, result) => {
+            if (err) {
+                console.log(err)
+                return
+            }
+            console.log(result)
+        })
+    })
+})
