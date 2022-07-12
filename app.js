c24ca3e64cbcb1b01c38a27ebfd0ea1bd2fd3eb4
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-07-11 22:59:09 -0700

    Fs built in module

--- app.js
+++ app.js
@@ -84,4 +84,7 @@ console.log(absolute)
 // sync
 const { readFileSync, writeFileSync } = require('fs')
 
+const first = readFileSync('./content/first.txt', 'utf8')
+const second = readFileSync('./content/second.txt', 'utf8')
 
+console.log(first, second)
