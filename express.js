faa7db0dca5a11b6773249c9c45cdae83dd1ef21
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-07-13 04:11:08 -0700

    express walkthrough

--- express.js
+++ express.js
@@ -3,13 +3,15 @@ const express = require('express')
 
 const app = express()
 
-// app.get
-// app.post
-// app.put
-// app.delete
-// app.all
-// app.use
-// app.listen
+// app.get -- read data
+// app.post -- insert data
+// app.put -- update data
+// app.delete -- delete data
+// app.all -- works with all of them
+// app.use -- for middleware
+// app.listen -- port to listen for communication
+
+
 
 app.listen(5005, () => {
     console.log("server running")
