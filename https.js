aa2c8747c072fb604f72e40532f66585e812dce7
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-07-13 01:35:19 -0700

    http walkthrough

--- https.js
+++ https.js
@@ -1,5 +1,22 @@
+// HTTP walkthrough
 const http = require('http')
 
 const server = http.createServer((req, res) => {
     console.log('user hit the server')
-}).listen(5000)
+    const url = req.url;
+    // console.log(req.method)
+    // console.log(req.url)
+    if ( url === '/'){
+        res.writeHead(200, { 'content-type': 'text/html' })
+        res.write('<h1>Home page</h1>') 
+        res.end() //signals communication is over
+    } else if( url === '/about') {
+        res.writeHead(200, { 'content-type': 'text/html' })
+        res.write('<h1>about page</h1>') 
+        res.end() //signals communication is over
+    } else {
+        res.writeHead(404, { 'content-type': 'text/html' })
+        res.write('<h1>Page not found</h1>') 
+        res.end() //signals communication is over
+    }
+}).listen(5004)
