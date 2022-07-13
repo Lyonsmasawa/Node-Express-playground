1a39495a6486ef947a834a1ac58aefbd54b2ddfc
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-07-13 05:04:55 -0700

    params and query string

--- public/express.js
+++ public/express.js
@@ -56,12 +56,13 @@ const app = express()
     const {products} = require('./data')
 
     app.get('/', (req, res) => {
-        res.send('<h1>Home Page </h1>')
+        res.send('<h1>Home Page </h1><a href="/api/products"/>Products</a>')
     })
-    app.get('/data', (req, res) => {
-        // res.json([{name:'Lyons'}, {name: 'Albert'}])
-        res.json(products)
-    })
+
+    // app.get('/data', (req, res) => {
+    //     // res.json([{name:'Lyons'}, {name: 'Albert'}])
+    //     res.json(products)
+    // })
 
     // Params, Query string
 
