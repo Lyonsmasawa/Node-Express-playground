e8040a7256a950b0efd15dd60738a170cebdf143
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-07-13 02:18:34 -0700

    http walkthrough

--- https.js
+++ https.js
@@ -1,5 +1,9 @@
 // HTTP walkthrough
 const http = require('http')
+const {readFileSync} = require('fs')
+
+// get all files
+const homePage = readFileSync('./http.html')
 
 const server = http.createServer((req, res) => {
     console.log('user hit the server')
@@ -8,7 +12,7 @@ const server = http.createServer((req, res) => {
     // console.log(req.url)
     if ( url === '/'){
         res.writeHead(200, { 'content-type': 'text/html' })
-        res.write('<h1>Home page</h1>') 
+        res.write(homePage) 
         res.end() //signals communication is over
     } else if( url === '/about') {
         res.writeHead(200, { 'content-type': 'text/html' })
