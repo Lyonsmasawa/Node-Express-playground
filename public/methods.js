54e836dfb1ea7daf92541f18801622287bfdff15
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-07-14 11:05:08 -0700

    GET method

--- public/methods.js
+++ public/methods.js
@@ -4,7 +4,9 @@ const app = express()
 let {people} = require('./data')
 
 // GET - read data
-
+app.get('/api/people', (req, res) => {
+    res.status(200).json({success: true, data:people})
+})
 
 app.listen(5008, () => {
     console.log('listening on 5008')
