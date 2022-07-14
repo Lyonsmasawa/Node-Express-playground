bb62ed6cff0f996f7401774bff0f82975e2c6d66
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-07-13 22:51:25 -0700

    middleware set up

--- public/express.js
+++ public/express.js
@@ -131,6 +131,10 @@ const app = express()
 // Middlewares - major
 // req => middleware => res
 app.get('/', (req, res) => {
+    const method = req.method
+    const url = req.url
+    const time = new Date().getFullYear()
+    console.log(method, url, time)
     res.send("home")
 })
 
