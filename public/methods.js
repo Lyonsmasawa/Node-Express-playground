cc0fbcb6258434bd5aad27f4a548a23685309694
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-07-14 11:29:31 -0700

    POST method

--- public/methods.js
+++ public/methods.js
@@ -1,10 +1,14 @@
 // HTTP METHODS
 const express = require('express')
 const app = express()
+const path = require('path')
 let {people} = require('./data')
 
 app.use(express.static('../public'))
 
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'./index.html'))
+})
 // GET - read data
 app.get('/api/people', (req, res) => {
     res.status(200).json({success: true, data:people})
