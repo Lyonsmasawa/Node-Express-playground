28e4bda636487414f4d82aaa20572d8b1e053cb2
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-07-13 22:59:03 -0700

    middleware set up

--- public/express.js
+++ public/express.js
@@ -130,15 +130,25 @@ const app = express()
 
 // Middlewares - major
 // req => middleware => res
-app.get('/', (req, res) => {
-    const method = req.method
-    const url = req.url
-    const time = new Date().getFullYear()
-    console.log(method, url, time)
+    // make it a func - set up middleware
+    const logger = (req, res, next) => { //express supplies the re and res ao we just have to pass them as parameters
+        const method = req.method
+        const url = req.url
+        const time = new Date().getFullYear()
+        console.log(method, url, time)
+        next() //-- when you work with middleware you have to pass the baton to the next function unless you are terminating by sending the result yourself (a must)
+    }
+
+app.get('/', logger, (req, res) => {
+        // manual without middleware illustration --->
+            // const method = req.method
+            // const url = req.url
+            // const time = new Date().getFullYear()
+            // console.log(method, url, time)
     res.send("home")
 })
 
-app.get('/about', (req, res) => {
+app.get('/about', logger, (req, res) => {
     res.send("about")
 })
 
