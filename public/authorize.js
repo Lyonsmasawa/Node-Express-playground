1e061b8e5f3395a8a6d20f97d9d9b363ff7bf7e5
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-07-14 10:47:18 -0700

    middleware fucntions

--- public/authorize.js
+++ public/authorize.js
@@ -1,4 +1,11 @@
 const authorize = (req, res, next) => {
+    const {user} = req.query;
+    if (user === 'john') {
+        req.user = { name:'lyons', id:3 }
+        next()
+    } else {
+        res.status(401).send('unauthorized')
+    }
     console.log('authorize')
     next()
 }
