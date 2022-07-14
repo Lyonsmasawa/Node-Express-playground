ef1c951f5110199170a195231cc72343f56c8974
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-07-14 11:03:14 -0700

    GET

--- public/methods.js
+++ public/methods.js
@@ -1,3 +1,11 @@
 // HTTP METHODS
+const express = require('express')
+const app = express()
+let {people} = require('./data')
+
     // GET - read data
 
+
+app.listen(5008, () => {
+    console.log('listening on 5008')
+})
