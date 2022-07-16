24e9f5360ce1fec4ea801dad299ba3c251da9434
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-07-16 13:45:48 -0700

    POST method using js

--- public/post.js
+++ public/post.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+
+app.use(express.static(path.resolve(__dirname, './post-js.html')))
+
+
 
 app.listen(5009, () => {
     console.log('listening on 5009')
