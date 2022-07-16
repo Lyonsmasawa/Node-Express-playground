0ddd1ac70478f0ebaa91943ff5f60d91ab9f2ab7
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-07-16 16:00:10 -0700

    controllers in express

--- public/routes/people.js
+++ public/routes/people.js
@@ -4,3 +4,7 @@
 // router.get('/')
 // module.exports = router - export
 // import= "app.use('api/people', people) -base url plus the router(remove the base url from the routes)
+
+// controllers 
+// create a folder - controllers
+// 
