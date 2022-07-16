5a13a2600da3a30da34717efb031458d5071b392
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-07-16 15:54:59 -0700

    ROUTES in express

--- public/routes/people.js
+++ public/routes/people.js
@@ -1,2 +1,6 @@
 const express = require('express')
 router = express.Router()
+
+// router.get('/')
+// module.exports = router - export
+// import= "app.use('api/people', people) -base url plus the router t
